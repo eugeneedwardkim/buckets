31ad8c7a6aec35de658c6dcc444d1e643e2c1a4b
Eugene Kim <eugene.edward.kim@gmail.com>
2017-02-23 19:23:24 -0700

    I did some things

--- routes/cups.js
+++ routes/cups.js
@@ -1,3 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const Bowl = require('../models/bowl');
+const Cup = require('../models/cup');
+
+
+router.get('/', (req, res) =>{
+  Cup.find({ bowlId: req.query.bowlId }, (err, bowls) => {
+    res. json(cups);
+  });
+});
+
+router.post('/', (req, res) => {
+  let {bowlId, name} = req.body;
+  new Cup({
+    name,
+    bowlId
+  }).save(err, bowl) => {
+    res.json(bowl);
+  });
+});
+
+router.delete('./:id', (req, res) => {
+  Cup.findById(req.params.id, (err, cup) =>{
+    cup.remove();
+    res.status(200).send({succes: true});
+  });
+});
+
+module.exports = router;
